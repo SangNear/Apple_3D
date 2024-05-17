574dfc2743cca7bd1575f7eaeb8c03031c486804
Sang Near <nguyenlamsang2004@gmail.com>
2024-05-17 23:32:22 +0700

    done hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
+import React, { useEffect, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
+  const [video, setVideo] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const videoSetSrc = () => {
+    if (window.innerWidth < 760) {
+      setVideo(smallHeroVideo);
+    } else {
+      setVideo(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", videoSetSrc);
+
+    return () => {
+      window.removeEventListener("resize", videoSetSrc);
+    };
+  }, []);
   useGSAP(() => {
     gsap.to("#hero-title", {
       opacity: 1,
-      delay: 1
+      delay: 1,
+    });
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 1.5
     })
-  }, [])
+  }, []);
   return (
-    <section className='w-full nav-height bg-black relative'>
-      <div className='h-5/6 w-full '>
-        <p id='hero-title' className='text-3xl text-gray max-md:mb-10 font-semibold opacity-0 flex-center flex-col'>IPHONE 15 PRO</p>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p
+          id="hero-title"
+          className="text-3xl text-gray max-md:mb-10 font-semibold opacity-0 flex-center flex-col"
+        >
+          IPHONE 15 PRO
+        </p>
+        <div className="md:w-10/12 w-9/12">
+        <video autoPlay muted key={video}>
+          <source src={video}></source>
+        </video>
+        </div>
+        
+        <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+          <a href="#highlights" className="btn">Buy</a>
+          <p className="font-normal text-xl">From $199/ month or $999</p>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
 
 const Highlight = () => {
+  useGSAP(() => {
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25
+    })
+  },[])
   return (
-    <div>Highlight</div>
-  )
-}
+    <section id="highlights" className="h-full w-screen bg-zinc common-padding">
+      <div className="screen-max-width">
+        <div className="w-full flex mb-12 items-end justify-between">
+          <h1
+            id="title"
+            className="text-gray font-medium lg:text-6xl md:text-5xl text-3xl "
+          >
+            The highlights
+          </h1>
+          <div className="flex flex-center gap-5">
+            <p className="link">watch the film</p>
+            <p className="link">watch the event</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Highlight
+export default Highlight;
